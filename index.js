e2b632eaadeae30f805a792d285be2ac00638555
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-05 18:47:58 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+
+let newArray = [];
+
+const writeCards = (name, eventname) => {
+    for (let i = 0; i < name.length; i++) {
+        let message = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+        newArray.push(message);
+    }
+    return newArray;
+}
+
+const countDown = integer => {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
